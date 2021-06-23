66c502b91e54798d303dc2a4f3fe4ca6756e3c30
Piotr Płaczek <piotr.placzek.91@gmail.com>
2021-06-23 11:51:57 +0200

    command

--- src/Command/index.ts
+++ src/Command/index.ts
@@ -1,1 +1,35 @@
 /** https://refactoring.guru/pl/design-patterns/command */
+
+export interface Command {
+    execute(): void;
+}
+
+export class Button {
+    constructor(private readonly cmd: Command) {}
+    click(): void {
+        this.cmd.execute();
+    }
+}
+
+export class SaveFileCommand implements Command {
+    execute(): void {
+        console.log("saved");
+    }
+}
+
+export class RemoveFileCommand implements Command {
+    execute(): void {
+        console.log("removed");
+    }
+}
+
+/** USAGE */
+export function main(): void {
+    const saveCmd: Command = new SaveFileCommand();
+    const saveBtn: Button = new Button(saveCmd);
+    saveBtn.click();
+
+    const removeCmd: Command = new RemoveFileCommand();
+    const removeBtn: Button = new Button(removeCmd);
+    removeBtn.click();
+}
