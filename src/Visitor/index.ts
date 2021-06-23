95d85210f421131dde65cff7aa6600880464b82d
Piotr Płaczek <piotr.placzek.91@gmail.com>
2021-06-23 12:34:55 +0200

    visitor

--- src/Visitor/index.ts
+++ src/Visitor/index.ts
@@ -1,1 +1,30 @@
 /** https://refactoring.guru/pl/design-patterns/visitor */
+
+export interface Visitor {
+    visit(order: Order);
+}
+
+export class Order {
+    constructor(private readonly _price: number) {}
+    price(): number {
+        return this._price;
+    }
+
+    accept(visitor: Visitor): void{
+        visitor.visit(this);
+    }
+}
+
+export class PlainTextExporter implements Visitor{
+    visit(order: Order) {
+        console.log("order exported:", order);
+    }
+}
+
+/** USAGE */
+export function main(): void {
+    const exporter: PlainTextExporter = new PlainTextExporter();
+    const order: Order = new Order(123);
+
+    order.accept(exporter);
+}
